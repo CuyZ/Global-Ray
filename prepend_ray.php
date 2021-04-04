db50b2c0d34d584fdb49d4f3164ab7c2b60c0146
Romain Canon <romain.hydrocanon@gmail.com>
2021-04-04 21:54:22 +0200

    Fix: Resolve potential autoloading conflicts

--- prepend_ray.php
+++ prepend_ray.php
@@ -4,4 +4,10 @@ $globalRayAutoloader = __DIR__ . '/build/vendor/scoper-autoload.php';
 
 if (file_exists($globalRayAutoloader)) {
     require_once $globalRayAutoloader;
+
+    // This empties the global configuration used in Composer autoloader file
+    // `autoload_real.php`. Without it, conflicts may appear with projects using
+    // same dependencies as this package.
+    // Inspired by https://github.com/phpstan/phpstan-src/commit/5e66c6c
+    $GLOBALS['__composer_autoload_files'] = [];
 }
