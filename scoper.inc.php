abfeb553525043bd3dfe0aa074eb595a128d5322
Romain Canon <romain.hydrocanon@gmail.com>
2021-04-04 22:19:59 +0200

    Fix: Resolve conflicts with root package autoloader

--- scoper.inc.php
+++ scoper.inc.php
@@ -24,7 +24,22 @@ return [
     ],
     'whitelist' => [
         'ray',
-        'rd'
+        'rd',
+    ],
+    'patchers' => [
+        function (string $filePath, string $prefix, string $content): string {
+            // Adding configuration to `composer.json` file that will override
+            // the autoloader suffix, otherwise it will conflict with the one
+            // from this package.
+            if ($filePath === __DIR__ . '/composer.json') {
+                $json = json_decode($content, true);
+                $json['config']['autoloader-suffix'] = 'RayGlobalScoped';
+
+                return json_encode($json);
+            }
+
+            return $content;
+        },
     ],
     'whitelist-global-constants' => true,
     'whitelist-global-classes' => false,
